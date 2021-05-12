ae05d31fdae8a6b11814ef2c99f528e44d6eb662
SNmcdarby1 <sngereso@gmail.com>
2021-05-12 23:27:40 +0000

    edit static load block

--- xacade/settings.py
+++ xacade/settings.py
@@ -165,4 +165,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
